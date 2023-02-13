346a3fc981a329e91e4ddc0a14897b44c20d5f96
Okhan2021 <omar@allclear.ai>
2023-02-13 11:01:30 -0500

    added front end api

--- client/src/mods/UserForm.js
+++ client/src/mods/UserForm.js
@@ -1,31 +1,43 @@
+import React,{useEffect,useState} from 'react'
 let UserForm=()=>{
-    let submitHandler=(e)=>{
-        e.preventDefault()
-        console.log(e.target[0].value)
-        console.log(e.target[1].value)
-        let userData={
+    let [obj,setObj]=useState('')
+
+    let submitHandler=async (e)=>{
+        await e.preventDefault()
+        let userData=await {
             name:e.target[0].value,
             msg:e.target[1].value
         }
-        // FormPostHandler(userData)
-    }
-    let FormPostHandler=(userData)=>{
-        fetch('http://127.0.0.1:', {
-            Method: 'POST',
-            Headers: {
-              Accept: 'application.json',
-              'Content-Type': 'application/json'
-            },
-            Body: userData
-          })
+        await FormPostHandler(userData)
+        // await getData()
+        // await console.log(obj)
     }
 
+    let FormPostHandler= async (userData)=>{
+        await fetch('http://localhost:5258/api/Demo',{
+                method:'POST',
+                headers:{
+                   'Content-Type':'application/json'
+                },
+                body:JSON.stringify(userData)
+            })
+
+    }
+    let getData= async()=> {
+        const actualData = await fetch('http://localhost:5258/api/Demo/4')
+        let jsonActualData=await actualData.json()
+        await setObj(jsonActualData)
+      }
+
     return(
+        <React.Fragment>
         <form onSubmit={submitHandler}>
             <input type='text'/>
             <input type='textbox'/>
             <input type='submit'/>
         </form>
+        <p>{obj}</p>
+        </React.Fragment>
     )
 }
 export default UserForm
